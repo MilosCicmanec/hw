669615ca44c908b8279acb04a56d233be72edc69
MilosCicmanec <171834077+MilosCicmanec@users.noreply.github.com>
2026-08-16 13:59:39 +0200

    super block setup is finished

--- fs.cpp.cpp
+++ fs.cpp.cpp
@@ -27,13 +27,15 @@ fstream file;
 
 void sup_init() {
   memset(sb.name, 0, sizeof(sb.name));
+  strncpy(sb.name, "MyFs", sizeof(sb.name) - 1);
+
   sb.block_count = 1024;
   sb.block_size = 512;
   sb.fat_start_block = 1;
 
   ll tmp = (sb.block_count * 4 + (sb.block_size - 1)) / sb.block_size;
   sb.data_start_block = tmp + 1;
-};
+}
 int open_file(const string &filename) {
   file.open(filename, ios::in | ios::out | ios::binary);
   if (!file.is_open()) {
@@ -41,12 +43,30 @@ int open_file(const string &filename) {
   }
   return 0;
 }
+void wipe_disk() {
+  file.seekp(0);
+  vector<char> zero_block(sb.block_size, 0);
+  for (int i = 0; i < sb.block_count; i++) {
+    file.write(zero_block.data(), sb.block_size);
+  };
+  file.flush();
+  cout << "Disk zeroed out completely" << endl;
+}
+
 int main() {
+  // disk init
   if (open_file("archive.bin") == 0) {
     cout << "File opened" << endl;
   } else {
     cout << "File opening error" << endl;
   }
+  sup_init();
+  wipe_disk(); // this should probably be optional
+
+  // write the sperblock data onto the disk
+  file.seekp(0);
+  file.write(reinterpret_cast<char *>(&sb), sizeof(Superblock));
+  file.flush();
 
   return 0;
 }
